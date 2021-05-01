4fae6dc6b4666b575fec57101a6fc46d56ecf445
Gabriel Franzeri <gabrielfranzer@gmail.com>
2021-05-01 16:32:13 -0300

    abrir navegador

---
+++ Envia_Texto_Diario.py
@@ -0,0 +1,8 @@
+# Programa para envio do Texto Di�rio no Email 
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# Para abrir o navegador
+nav = webdriver.Chrome()
+
