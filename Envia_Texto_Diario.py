e4ee94ba51875e093958d0088b2341b6192a13df
Gabriel Franzeri <gabrielfranzer@gmail.com>
2021-05-01 21:45:12 -0300

    apaguei o for para envio de emails, próximo passo, juntar os email numa string única para fazer o envio

--- Envia_Texto_Diario.py
+++ Envia_Texto_Diario.py
@@ -37,6 +37,7 @@ email = outlook.CreateItem(0) # cria item no outlook
 
 # configurar as informações do email
 # email.To = email_destino.loc[0, 'email'] # destino
+
 email.Subject = texto[0] # assunto
 email.HTMLBody = f"""
 <body style="background-image: linear-gradient(#000272, #341677, #a32f80, #ff6363);">
@@ -48,6 +49,7 @@ email.HTMLBody = f"""
     <p style="color: #51d2d6; font-family: Verdana, Geneva, Tahoma, sans-serif;">{texto[3]}</p>
 </body>
 """ # corpo do email'
+
 # anexo no email
 # anexo = "caminho do anexo"
 # email.Attachments.Add(anexo)
@@ -55,10 +57,10 @@ email.HTMLBody = f"""
 # email destino
 import pandas as pd
 
-email_destino = pd.read_csv('email.csv') # 'email@destino.com'
+# email_destino = pd.read_csv('email.csv') # 'email@destino.com'
 
-for index, row in email_destino.iterrows():
-    email.To = row['email']
-    # Enviar email
-    email.Send()
-    print(f'Email Enviado para {row['email']}')
+# for index, row in email_destino.iterrows():
+#     email.To = row['email']
+#     # Enviar email
+#     email.Send()
+#     print(f'Email Enviado para {row}')
